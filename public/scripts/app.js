f63226edc8ec5e0797dcf75632c15609bed23c99
Ardelia Tay <ardeliatay@Ardelias-MacBook-Pro.local>
2018-10-21 18:20:18 -0700

    projects with toggle button to show details

---
+++ public/scripts/app.js
@@ -0,0 +1,49 @@
+$(function() {
+  $('#jungle-toggle').hide();
+
+  //New tweet form slides down when compose button is clicked
+  $('#jungle-button').click(function() {
+    $('#jungle-toggle').slideToggle();
+  });
+
+});
+
+$(function() {
+  $('#chatty-toggle').hide();
+
+  //New tweet form slides down when compose button is clicked
+  $('#chatty-button').click(function() {
+    $('#chatty-toggle').slideToggle();
+  });
+
+});
+
+$(function() {
+  $('#mindspace-toggle').hide();
+
+  //New tweet form slides down when compose button is clicked
+  $('#mindspace-button').click(function() {
+    $('#mindspace-toggle').slideToggle();
+  });
+
+});
+
+$(function() {
+  $('#tweeter-toggle').hide();
+
+  //New tweet form slides down when compose button is clicked
+  $('#tweeter-button').click(function() {
+    $('#tweeter-toggle').slideToggle();
+  });
+
+});
+
+$(function() {
+  $('#tinyapp-toggle').hide();
+
+  //New tweet form slides down when compose button is clicked
+  $('#tinyapp-button').click(function() {
+    $('#tinyapp-toggle').slideToggle();
+  });
+
+});
